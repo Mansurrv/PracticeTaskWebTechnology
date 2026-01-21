920de42f109634f25f3ae4ecaf862e2ba568af90
mansurrv <buyserikov06@gmail.com>
2026-01-21 19:31:48 +0500

    Second Commit

--- practice8.js
+++ practice8.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+require('dotenv').config();
 
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect("mongodb://127.0.0.1:27017/shop")
+mongoose.connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
